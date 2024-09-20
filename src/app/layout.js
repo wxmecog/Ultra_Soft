24ab9ee740e69ea84449830eb00f592cb7f4b089
Elite <elitedevsolution@gmail.com>
2024-09-20 14:20:44 +0200

    update: favicon, manifest, logo, splash

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,17 +17,17 @@ import { AuthProvider } from 'src/auth/context/jwt';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-  title: 'Minimal UI Kit',
+  title: 'Ultra Software Labs',
   description:
-    'The starting point for your next project with Minimal UI Kit, built on the newest version of Material-UI ©, ready to be customized to your style',
-  keywords: 'react,material,kit,application,dashboard,admin,template',
+    'Ultra Software Labs is a leading software, website, database, mobile and e-commerce development company. With a passion for innovation and a commitment to delivering top-notch solutions, we empower organizations to thrive in the digital landscape.s',
+  keywords: 'ultra, soft, labs',
   themeColor: '#000000',
   manifest: '/manifest.json',
   viewport: { width: 'device-width', initialScale: 1, maximumScale: 1 },
   icons: [
-    { rel: 'icon', url: '/favicon/favicon.ico' },
-    { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/favicon/favicon-16x16.png' },
-    { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/favicon/favicon-32x32.png' },
+    { rel: 'icon', url: '/logo/logo_single.svg' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', url: '/logo/logo_single.svg' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', url: '/logo/logo_single.svg' },
     { rel: 'apple-touch-icon', sizes: '180x180', url: '/favicon/apple-touch-icon.png' },
   ],
 };
--- src/components/loading-screen/splash-screen.js
+++ src/components/loading-screen/splash-screen.js
@@ -66,7 +66,7 @@ export default function SplashScreen({ sx, ...other }) {
             width: 100,
             height: 100,
             position: 'absolute',
-            border: (theme) => `solid 3px ${alpha(theme.palette.primary.dark, 0.24)}`,
+            border: (theme) => `solid 3px ${alpha(theme.palette.ultra.dark, 0.24)}`,
           }}
         />
 
@@ -87,7 +87,7 @@ export default function SplashScreen({ sx, ...other }) {
             width: 120,
             height: 120,
             position: 'absolute',
-            border: (theme) => `solid 8px ${alpha(theme.palette.primary.dark, 0.24)}`,
+            border: (theme) => `solid 8px ${alpha(theme.palette.ultra.dark, 0.24)}`,
           }}
         />
       </>
